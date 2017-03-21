8512cfeedb81262c312641811ac81012453b73ab
liutsingluo <liutsingluo@163.com>
2017-03-21 16:04:54 +0800

    use mongodb

--- resources/views/themes/feiyang/common.blade.php
+++ resources/views/themes/feiyang/common.blade.php
@@ -8,6 +8,109 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="{{homeAssets('/images/favicon.ico')}}" rel="shortcut icon">
     <link href="{{homeAssets('/css/screen.css')}}" media="screen, projection" rel="stylesheet" type="text/css">
+    <style>
+        #sub-nav .social a:hover {
+            opacity: 1
+        }
+
+        #sub-nav .social a.facebook {
+            background: url({{homeAssets('/images/social/facebook.png')}}) center no-repeat #3b5998;
+            border: 1px solid #3B5998
+        }
+
+        #sub-nav .social a.facebook:hover {
+            border: 1px solid #2d4373
+        }
+
+        #sub-nav .social a.google {
+            background: url({{homeAssets('/images/google.png')}}) center no-repeat #c83d20;
+            border: 1px solid #C83D20
+        }
+
+        #sub-nav .social a.google:hover {
+            border: 1px solid #9c3019
+        }
+
+        #sub-nav .social a.twitter {
+            background: url({{homeAssets('/images/twitter.png')}}) center no-repeat #55cff8;
+            border: 1px solid #55CFF8
+        }
+
+        #sub-nav .social a.twitter:hover {
+            border: 1px solid #24c1f6
+        }
+
+        #sub-nav .social a.github {
+            background: url({{homeAssets('/images/github.png')}}) center no-repeat #afb6ca;
+            border: 1px solid #afb6ca
+        }
+
+        #sub-nav .social a.github:hover {
+            border: 1px solid #909ab6
+        }
+
+        #sub-nav .social a.coderwall {
+            background: url({{homeAssets('/images/coderwall.png')}}) center no-repeat #3a729f;
+            border: 1px solid #3a729f
+        }
+
+        #sub-nav .social a.coderwall:hover {
+            border: 1px solid #2c577a
+        }
+
+        #sub-nav .social a.pinboard {
+            background: url({{homeAssets('/images/pinboard.png')}}) center no-repeat #0066c8;
+            border: 1px solid #3a729f
+        }
+
+        #sub-nav .social a.pinboard:hover {
+            border: 1px solid #0052cc
+        }
+
+        #sub-nav .social a.linkedin {
+            background: url({{homeAssets('/images/linkedin.png')}}) center no-repeat #005a87;
+            border: 1px solid #005A87
+        }
+
+        #sub-nav .social a.linkedin:hover {
+            border: 1px solid #003854
+        }
+
+        #sub-nav .social a.pinterest {
+            background: url({{homeAssets('/images/pinterest.png')}}) center no-repeat #be4037;
+            border: 1px solid #be4037
+        }
+
+        #sub-nav .social a.pinterest:hover {
+            border: 1px solid #96332c
+        }
+
+        #sub-nav .social a.delicious {
+            background: url({{homeAssets('/images/delicious.png')}}) center no-repeat #3271cb;
+            border: 1px solid #3271cb
+        }
+
+        #sub-nav .social a.delicious:hover {
+            border: 1px solid #285aa2
+        }
+
+        #sub-nav .social a.rss {
+            background: url({{homeAssets('/images/rss.png')}}) center no-repeat #ef7522;
+            border: 1px solid #EF7522
+        }
+
+        #sub-nav .social a.rss:hover {
+            border: 1px solid #cf5d0f
+        }
+
+        @font-face {
+            font-family: 'FontAwesome';
+            src: url({{homeAssets('/font/fontawesome-webfont.eot')}});
+            src: url({{homeAssets('/font/fontawesome-webfont.eot?#iefix')}}) format("embedded-opentype"), url({{homeAssets('/font/fontawesome-webfont.woff')}}) format("woff"), url({{homeAssets('/font/fontawesome-webfont.ttf')}}) format("truetype"), url({{homeAssets('/font/fontawesome-webfont.svgz#FontAwesomeRegular')}}) format("svg"), url({{homeAssets('/font/fontawesome-webfont.svg#FontAwesomeRegular')}}) format("svg");
+            font-weight: normal;
+            font-style: normal
+        }
+    </style>
     <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
--- resources/views/themes/feiyang/content.blade.php
+++ resources/views/themes/feiyang/content.blade.php
@@ -8,7 +8,7 @@
         <article class="post">
             <h2 class="title">
                 <a href="/2014/05/handle-php-error-better-introduce-errors">
-                    更好的PHP错误处理：常规错误处理介绍</a>
+                    我的网站测试中。。。</a>
             </h2>
             <div class="entry-content">
                 <p>本来这篇博文准备早点发布，没想到拖延症还是没有改善，但对于责任心很强的人类来说，这始终是个压力。写这个的时候还在灰机上，我想下飞机就可以释放一下了。</p>
@@ -16,7 +16,7 @@
                 <h2>错误类型</h2>
                 <p>PHP 主要有两种错误：触发错误和异常。其中触发错误大概可以分为：编译错误、引擎错误和运行时错误，其中前两个是无法捕获的；异常都是可以捕获的，当没有尝试捕获时则会中断代码。</p>
                 <p>触发错误可以通过 <code>error_get_last()</code> 来进行获得，异常可以使用标准的 <code>try...catch</code> 语句来捕获。</p>
-                <a href="/2014/05/handle-php-error-better-introduce-errors" class="more-link">Read on &rarr;</a>
+                <a href="#" class="more-link">Read on &rarr;</a>
             </div>
             <div class="meta">
                 <div class="date">
@@ -30,8 +30,8 @@
             </div>
         </article>
         <nav id="pagenavi">
-            <a href="/blog/page/2" class="next">Next</a>
-            <div class="center"><a href="/blog/archives">Blog Archives</a></div>
+            <a href="#" class="next">Next</a>
+            <div class="center"><a href="#">Blog Archives</a></div>
         </nav>
     </div>
 @endsection

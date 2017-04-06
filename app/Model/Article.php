8643890b8a6fe060eeec2731313f4637931d450f
root <root@localhost.localdomain>
2017-04-06 09:26:14 -0400

    use mongodb and test success

---
+++ app/Model/Article.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stanley
+ * Date: 2017/4/6 0006
+ * Time: 21:00
+ */
+
+namespace app\Model;
+
+
+class Article
+{
+
+}
---
+++ resources/views/themes/feiyang/detail.blade.php
@@ -0,0 +1,15 @@
+@extends('themes.feiyang.common')
+@inject('article', 'App\Presenters\ArticlePresenter')
+@section('title')
+    枫叶映像
+@endsection
+@section('content')
+    <div id="content" class="inner">
+        <!--文章列表-->
+        {!! $article->article($data) !!}
+        <nav id="pagenavi">
+            <a href="#" class="next">Next</a>
+            <div class="center"><a href="#">Blog Archives</a></div>
+        </nav>
+    </div>
+@endsection

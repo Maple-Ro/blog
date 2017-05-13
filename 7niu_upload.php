a88b1187c0867115821217b35d9cc48e6ebc7109
liutsingluo <liutsingluo@163.com>
2017-05-13 18:28:57 +0800

    add 7niu storage

--- 7niu_upload.php
+++ 7niu_upload.php
@@ -16,7 +16,7 @@ $token = $auth->uploadToken($bucket);
 // 要上传文件的本地路径
 $filePath = '/var/log/mongodb/blog.tar.gz';
 // 上传到七牛后保存的文件名
-$key = 'backup.tar.gz';
+$key = "backup".date('ymd').".tar.gz";
 // 初始化 UploadManager 对象并进行文件的上传。
 $uploadMgr = new UploadManager();
 // 调用 UploadManager 的 putFile 方法进行文件的上传。
